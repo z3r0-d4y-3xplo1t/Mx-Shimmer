1b8ae6f6a669a9d245d67fe190544e003eb70c9a
z3r0.d4y.3xplo1t <68469601+z3r0-d4y-3xplo1t@users.noreply.github.com>
2021-08-22 15:59:57 +0700

    update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,5 @@
 function handler(m) {
-  // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281334177340', 'Nurutomo', m)
+  this.sendContact(m.chat, '16163190066', 'z3r0.d4y.3xplo1t', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
