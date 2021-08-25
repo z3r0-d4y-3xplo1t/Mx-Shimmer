243130dc0e0aace665c46eb2d923b3042316ab09
z3r0.d4y.3xplo1t <68469601+z3r0-d4y-3xplo1t@users.noreply.github.com>
2021-08-25 16:39:52 +0700

    Update asahotak_ans.js

--- plugins/asahotak_ans.js
+++ plugins/asahotak_ans.js
@@ -3,20 +3,23 @@ const threshold = 0.72
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*ao/i.test(m.quoted.text)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*ao/i.test(m.quoted.contentText)) return !0
     this.asahotak = this.asahotak ? this.asahotak : {}
     if (!(id in this.asahotak)) return m.reply('Soal itu telah berakhir')
-    if (m.quoted.id == this.asahotak[id][0].id) {
-        let json = JSON.parse(JSON.stringify(this.asahotak[id][1]))
-        // m.reply(JSON.stringify(json, null, '\t'))
-        if (m.text.toLowerCase() == json.result.jawaban.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.asahotak[id][2]
-            m.reply(`*Benar!*\n+${this.asahotak[id][2]} XP`)
-            clearTimeout(this.asahotak[id][3])
-            delete this.asahotak[id]
-        } else if (similarity(m.text.toLowerCase(), json.result.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-        else m.reply(`*Salah!*`)
+    if (/^nyerah$/i.test(m.text)) {
+        await this.sendButton(m.chat, `Jawabannya adalah ${JSON.parse(JSON.stringify(this.asahotak[id][1].jawaban))}`.trim(), '© stikerin', 'ASAH OTAK', '.asahotak').then(() => { delete this.asahotak[id] })
     }
+    // if (m.quoted.id == this.asahotak[id][0].id) {
+    let json = JSON.parse(JSON.stringify(this.asahotak[id][1]))
+    if (['.ao', 'BANTUAN', ''].includes(m.text)) return !0
+    if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
+        global.db.data.users[m.sender].exp += this.asahotak[id][2]
+        await this.sendButton(m.chat, `*Benar!* +${this.asahotak[id][2]} XP`, '© stikerin', 'ASAH OTAK', '.asahotak')
+        clearTimeout(this.asahotak[id][3])
+        delete this.asahotak[id]
+    } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
+    else m.reply(`*Salah!*`)
+    // }
     return !0
 }
 handler.exp = 0
