4446830b08450ddba17b6b1d09b6c8593333d9e8
z3r0.d4y.3xplo1t <68469601+z3r0-d4y-3xplo1t@users.noreply.github.com>
2021-08-27 10:09:58 +0700

    Update and rename enhance.js to TOOLS-HD.js

--- plugins/TOOLS-HD.js
+++ plugins/TOOLS-HD.js
@@ -16,7 +16,7 @@ let handler = async (m) => {
   if (res.status !== 200) throw await res.json()
   await conn.sendFile(m.chat, await res.buffer(), 'hd.jpg', 'Nih, hd kan?', m)
 }
-handler.help = ['hd (caption|reply media)', 'enhance (caption|reply media)']
+handler.help = ['hd <reply media>', 'enhance <reply media>']
 handler.tags = ['tools']
 handler.command = /^(hd|enhance)$/i
 
