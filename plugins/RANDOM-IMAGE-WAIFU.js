60b23b27dcc4f090ef8bd6fe9e5addbb05471539
z3r0.d4y.3xplo1t <68469601+z3r0-d4y-3xplo1t@users.noreply.github.com>
2021-09-07 13:34:05 +0700

    Update and rename waifu.js to RANDOM-IMAGE-WAIFU.js

--- plugins/RANDOM-IMAGE-WAIFU.js
+++ plugins/RANDOM-IMAGE-WAIFU.js
@@ -4,10 +4,10 @@ let handler = async(m, { conn }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'Istri kartun', m)
+  conn.sendFile(m.chat, json.url, '', '', m)
 }
 handler.help = ['waifu']
 handler.tags = ['internet']
 handler.command = /^(waifu)$/i
-//MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS
+
 module.exports = handler
